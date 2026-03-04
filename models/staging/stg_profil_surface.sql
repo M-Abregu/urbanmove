acf8377f595d751ef9c9198230e44602a0c6d8d3
marianaabregu1+dbt@gmail.com <marianaabregu1+dbt@gmail.com>
2026-03-04 10:51:16 +0000

    MAJ de requete

--- models/staging/stg_profil_surface.sql
+++ models/staging/stg_profil_surface.sql
@@ -1,8 +1,8 @@
 SELECT
-ANNEE,
-SEMESTRE,
-ID_GROUPOFLIGNE,
-CAT_JOUR,
+ANNEE as annee,
+SEMESTRE as semestre,
+trim(ID_GROUPOFLIGNE) as ID_group_ligne ,
+trim(CAT_JOUR) as cat_jour,
 split(TRNC_HORR_60, '-')[offset(0)] AS heure,
 pourc_validations
 FROM {{ source('UrbanMove_raw', 'profil_surface') }}
